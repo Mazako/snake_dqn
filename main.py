dd6825749b5681a0744fedf2c4bd3f6df7519143
michalmaziarz <michal.maziarz@fingo.net>
2026-09-14 10:44:18 +0200

    simple snake state

--- main.py
+++ main.py
@@ -3,12 +3,9 @@ from __future__ import annotations
 from collections import deque
 from dataclasses import dataclass
 from enum import Enum
+from random import Random
 
-
-@dataclass(frozen=True, slots=True, eq=True)
-class Entity:
-    x: int
-    y: int
+from random_set import RandomSet
 
 
 class Direction(Enum):
@@ -25,6 +22,19 @@ class Direction(Enum):
         dx, dy = self.delta
         other_dx, other_dy = other.delta
         return (dx, dy) == (-other_dx, -other_dy)
+
+
+@dataclass(frozen=True, slots=True, eq=True)
+class Entity:
+    x: int
+    y: int
+
+    def next_pos(self, direction: Direction, board_size: int):
+        dx, dy = direction.delta
+        return Entity(
+            x=(self.x + dx) % board_size,
+            y=(self.y + dy) % board_size,
+        )
 
 
 class Snake:
@@ -44,7 +54,9 @@ class Snake:
         self.direction = direction
         return True
 
-    def move(self, board_size: int, *, grow: bool = False) -> bool:
+    def move(
+        self, board_size: int, *, grow: bool = False
+    ) -> tuple[bool, Entity | None]:
         dx, dy = self.direction.delta
         next_head = Entity(
             x=(self.head.x + dx) % board_size,
@@ -53,7 +65,7 @@ class Snake:
 
         body = self.segments if grow else list(self.segments)[:-1]
         if next_head in body:
-            return False
+            return False, None
 
         self.segments.appendleft(next_head)
         self.occupied.add(next_head)
@@ -61,8 +73,9 @@ class Snake:
         if not grow:
             tail = self.segments.pop()
             self.occupied.remove(tail)
+            return True, tail
 
-        return True
+        return True, None
 
     def __contains__(self, item: Entity) -> bool:
         return item in self.occupied
@@ -72,17 +85,44 @@ class Game:
     epoch: int
     score: int
     snake: Snake
-    food: Entity | None
+    food: Entity
 
     def __init__(self, size: int):
         self.size = size
-        self.epoch = 0
         self.score = 0
+        self.epoch = 0
         self.snake = Snake(Entity(0, 0), Direction.RIGHT)
-        self.food = None
+        self.rng = Random()
+        self.free_coords_pool = RandomSet(
+            {Entity(x, y) for x in range(size) for y in range(size)}
+        )
+        for segment in self.snake.occupied:
+            self.free_coords_pool.discard(segment)
+        self.food = self.free_coords_pool.pop_random(self.rng)
 
-    def _spawn_food(self) -> None:
-        pass
+    def next_food(self):
+        self.food = self.free_coords_pool.pop_random(self.rng)
+
+    def change_direction(self, direction: Direction) -> bool:
+        return self.snake.change_direction(direction)
+
+    def tick(self) -> bool:
+        self.epoch += 1
+        next_head = self.snake.head.next_pos(self.snake.direction, self.size)
+        will_eat = next_head == self.food
+        moved, tail = self.snake.move(self.size, grow=will_eat)
+        if not moved:
+            return False
+
+        if tail is not None:
+            self.free_coords_pool.add(tail)
+        self.free_coords_pool.discard(next_head)
+
+        if will_eat:
+            self.score += 1
+            self.next_food()
+
+        return True
 
 
 def main():
